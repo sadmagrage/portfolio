c0b48233dd4d41dac6131a873c3e91180dca2d89
sadmagrage <95193077+sadmagrage@users.noreply.github.com>
2023-08-22 15:41:48 -0300

    just need some fixes

---
+++ js/projects.js
@@ -0,0 +1,17 @@
+export const projects = [{
+    name: "juninho cabeçote",
+    tecnologies: ["NodeJS", "MongoDB"],
+    image: "https://d2r9epyceweg5n.cloudfront.net/stores/002/815/230/products/design-sem-nome-191-8aeb712c872fa63c2d16773541755222-640-0.png",
+    status: true,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium reiciendis ipsam distinctio sint laborum eveniet dolore in dolor. Inventore odio exercitationem labore quo et unde voluptatem magni reiciendis amet!",
+    repository: "d",
+    runningLink: "d"
+}, {
+    name: "juninho cabeçote",
+    tecnologies: ["node.js", "mongodb"],
+    image: "https://d2r9epyceweg5n.cloudfront.net/stores/002/815/230/products/design-sem-nome-191-8aeb712c872fa63c2d16773541755222-640-0.png",
+    status: false,
+    description: "dmsakdopsadksaokds",
+    repository: "d",
+    runningLink: ""
+}];
--- js/script.js
+++ js/script.js
@@ -1,53 +1,142 @@
-var projectsSection = document.querySelector('.projects-section');
-var leftArrow = document.querySelector("#left-arrow");
-var showProjectsElement = document.getElementById('show_projects');
-var identityElement = document.querySelector("#identity");
+import { tecnologies } from "./tecnologies.js";
+import { projects } from "./projects.js";
 
-identityElement.innerHTML = `Meu nome é Victor Goya, tenho ${parseInt((new Date().getTime() - new Date(2003, 10, 7, 8).getTime())/365/24/3600/1000)} anos.`;
+const projectsSection = document.querySelector('.projects-section');
+const leftArrow = document.querySelector("#left-arrow");
+const showProjectsElement = document.getElementById('show_projects');
+const identityElement = document.querySelector("#identity");
+const tecnologiesElement = document.querySelector("#tecnologies");
 
-projectsSection.addEventListener('click', function() {
+identityElement.innerHTML = `Meu nome é Victor Goya, tenho ${parseInt((new Date().getTime() - new Date(2003, 10, 7, 8).getTime()) / 365 / 24 / 3600 / 1000)} anos.`;
+
+let tecnologiesText = "";
+
+tecnologies.map(item => {
+    tecnologiesText += `
+        <div class="tecnology">
+            <img class="tecnology-image" src="./images/${ item.imageName }" alt="${ item.title }">
+            <h5 class="tecnology-title">${ item.title }</h5>
+        </div>`;
+});
+
+tecnologiesElement.innerHTML = tecnologiesText;
+
+projectsSection.addEventListener('click', function () {
     showProjectsElement.classList.toggle("visible");
 });
 
-leftArrow.addEventListener('click', function() {
+leftArrow.addEventListener('click', function () {
     showProjectsElement.classList.toggle("visible");
 });
 
-let projectsSectionHTML = "<h3>Projetos</h3>";
+let projectsSectionHTML = "<h3 id='projetos-title'>Projetos</h3>";
 
-/* const getPropertyStyle = (identifier, property) => {
-    const element = document.querySelector(identifier);
+const counter = projects.length > 2 ? 2 : projects.length;
 
-    const heightStr = getComputedStyle(element)[property];
-    console.log(parseInt(heightStr.substring(0, heightStr.length - 2)));
-    return parseInt(heightStr.substring(0, heightStr.length - 2));
-}; */
-
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < counter; i++) {
     projectsSectionHTML += `
-    <div class="project-thumbnail">
-        <img class="project-image" src="./images/wallpaper.webp" alt="">
-    </div>
-    `;
+        <div class="project-thumbnail">
+            <img class="project-image" src="${ projects[i].image }" alt="">
+        </div>`;
 }
 
 document.querySelector(".projects-section").innerHTML = projectsSectionHTML;
 
 let projectsListHTML = "";
 
-for (let i = 0; i < 9; i++) {
-    projectsListHTML += `
-    <div class="project-item">
-        <img class="project-image" src="./images/wallpaper.webp" alt="">
-        <div class="project-info">
-            <div class="project-technologies">
-                <p>java</p>
-                <p>mysql</p>
-            </div>
-            <h2>nome</h2>
+const iterateTecnologies = (data) => {
+    let tecnologies = "";
+
+    data.tecnologies.map((item) => {
+        tecnologies += `
+        <div class="tecnology-info">
+            <img class="tecnology-icon" src="./images/${ item.toLowerCase() }.png" />
+            <p>${item}</p>
         </div>
-    </div>
+        `;
+    });
+
+    return tecnologies;
+}
+
+const statusIsUp = (status) => {
+    if (status) {
+        return `
+        <span class="isUp"></span>
+        <p>Em execução</p>
+        `
+    }
+
+    return `
+        <span class="isntUp"></span>
+        <p>Não está em execução</p>
     `;
 }
 
+const projectLinks = (item) => {
+    let linksElement = "";
+
+    if (item.runningLink.length > 0) {
+        linksElement += `<a class="repository-link" href="${ item.repository }" target="_blank" ><img class="repository-image" src="../images/github.png" /></a><a class="project-link" href="${ item.runningLink }" target="_blank" ><h5 class="project-link-text">Link do projeto</h5></a>`
+    }
+    else {
+        linksElement = `<a class="repository-link only-link" href="${ item.repository }" target="_blank" ><img class="repository-image" src="../images/github.png" /></a>`;
+    }
+
+    return linksElement;
+}
+
+projects.map(data => {
+    projectsListHTML += `
+        <div class="project-item">
+            <h4 class="project-name">${ data.name }</h4>
+            <img class="project-image" src="${ data.image }" alt="">
+            <div id="description" class="inactive">
+                <p class="description-text">${ data.description }</p>
+                <div class="description-links">
+                    ${ projectLinks(data) }
+                </div>
+            </div>
+            <div class="project-info">
+                <div class="project-technologies">
+                    ${ iterateTecnologies(data) }
+                </div>
+                <div class="status">
+                    ${ statusIsUp(data.status) }
+                </div>
+            </div>
+        </div>`;
+});
+
 document.querySelector(".projects-list").innerHTML = projectsListHTML;
+
+const projectItemElements = document.querySelectorAll(".project-item");
+
+projectItemElements.forEach(projectItemElement => {
+    projectItemElement.addEventListener("click", (e) => {
+
+        if (e.target.classList.contains("description-links") || e.target.closest(".description-links")) {
+            e.stopPropagation();
+            return;
+        }
+    
+        const element = e.currentTarget.querySelector(".inactive") != null ? e.currentTarget.querySelector(".inactive") : e.currentTarget.querySelector(".active") ;
+    
+        element.classList.toggle("inactive");
+        element.classList.toggle("active");
+    });
+});
+
+window.addEventListener('resize', function () {
+    const mediaQuery768px = window.matchMedia('(max-width: 768px)');
+    const introSection = document.querySelector('.intro-section');
+    const tecnologiesContainer = document.querySelector('#tecnologies-container');
+
+    if (mediaQuery768px.matches) {
+        document.querySelector('.content').appendChild(tecnologiesContainer);
+    } else {
+        introSection.appendChild(tecnologiesContainer);
+    }
+});
+
+window.dispatchEvent(new Event('resize'));
