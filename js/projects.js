ecb368771fb50d1ab34b857be9c836cda6d23f9a
sadmagrage <95193077+sadmagrage@users.noreply.github.com>
2023-08-23 23:05:21 -0300

    finished

--- js/projects.js
+++ js/projects.js
@@ -1,12 +1,20 @@
 export const projects = [{
     name: "juninho cabeçote",
     tecnologies: ["NodeJS", "MongoDB"],
-    image: "https://d2r9epyceweg5n.cloudfront.net/stores/002/815/230/products/design-sem-nome-191-8aeb712c872fa63c2d16773541755222-640-0.png",
+    image: "https://http2.mlstatic.com/D_NQ_NP_732261-MLB42725229650_072020-O.webp",
     status: true,
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium reiciendis ipsam distinctio sint laborum eveniet dolore in dolor. Inventore odio exercitationem labore quo et unde voluptatem magni reiciendis amet!",
     repository: "d",
     runningLink: "d"
 }, {
+    name: "juninho cabeçote",
+    tecnologies: ["node.js", "mongodb"],
+    image: "https://d2r9epyceweg5n.cloudfront.net/stores/002/815/230/products/design-sem-nome-191-8aeb712c872fa63c2d16773541755222-640-0.png",
+    status: false,
+    description: "dmsakdopsadksaokds",
+    repository: "d",
+    runningLink: ""
+},{
     name: "juninho cabeçote",
     tecnologies: ["node.js", "mongodb"],
     image: "https://d2r9epyceweg5n.cloudfront.net/stores/002/815/230/products/design-sem-nome-191-8aeb712c872fa63c2d16773541755222-640-0.png",
